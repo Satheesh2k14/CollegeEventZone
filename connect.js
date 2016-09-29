7ebee869f438f753c4fe268facc1806e8bee7918
Satheesh2k14 <satheesh.prabhakaran97@gmail.com>
2016-09-29 18:21:30 +0530

    Student can post

--- connect.js
+++ connect.js
@@ -3,7 +3,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
+//tables
 var users;
+var events;
+//variables
 var type;
 var numItems;
 app.use(express.static(path.join(__dirname, './')));
@@ -19,7 +22,7 @@ mongodb.MongoClient.connect(uri, function(err, db) {
   
 	if(err) throw err;
 	users = db.collection('users');
- 
+ 	events = db.collection('events');
  	// / table
 
 	app.get('/', function (req, res) {
@@ -67,6 +70,81 @@ mongodb.MongoClient.connect(uri, function(err, db) {
 				res.status('500').send('showAlert');
 			}
 		});
+	});
+
+	//STUDENT SEMINAR POST 
+	app.post('/seminarpost', function(req,res){
+		console.log('trying to enter event');
+		events.find({ "_id" : req.body._id}).count().then(function(numItems){
+			console.log(numItems);
+			if(!numItems){
+					var eventBody = {
+						"_id" : req.body._id ,
+						 "type" : req.body.type
+					}
+					console.log(eventBody);
+					events.insert(eventBody, function(err, result){
+				    if(err){
+				    	throw err;
+				 	}
+				  }); 
+			}
+			else
+			{
+				console.log('Seminar already Exists');
+				res.status('500').send('showSeminar');
+			}
+		});	
+	});
+
+	//ShORT FILM POST
+	app.post('/sfpost', function(req,res){
+		console.log('trying to enter event');
+		events.find({ "_id" : req.body._id}).count().then(function(numItems){
+			console.log(numItems);
+			if(!numItems){
+					var eventBody = {
+						"_id" : req.body._id ,
+						 "type" : req.body.type
+					}
+					console.log(eventBody);
+					events.insert(eventBody, function(err, result){
+				    if(err){
+				    	throw err;
+				 	}
+				  }); 
+			}
+			else
+			{
+				console.log('Short film already Exists');
+				res.status('500').send('showSeminar');
+			}
+		});	
+	});
+
+	//PROJECT POST
+	app.post('/projectpost', function(req,res){
+		console.log('trying to enter project');
+		events.find({ "_id" : req.body._id}).count().then(function(numItems){
+			console.log(numItems);
+			if(!numItems){
+					var eventBody = {
+						"_id" : req.body._id ,
+						 "type" : req.body.type
+					}
+					console.log(eventBody);
+					events.insert(eventBody, function(err, result){
+				    if(err){
+				    	throw err;
+				 	}
+				  }); 
+			}
+			else
+			{
+				console.log('Project already Exists');
+				res.status('500').send('showSeminar');
+			}
+		});	
 	});
  
 });
